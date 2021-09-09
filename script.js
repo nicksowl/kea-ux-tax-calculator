898889a912a5bc53ad565296eb40e4ca3364281c
Nikolai Baida <nicksowl@pro-hole.local>
2021-09-09 14:10:04 +0200

    version 1

---
+++ script.js
@@ -0,0 +1,28 @@
+function calculateTax() {
+    let income = document.getElementById('money').value;
+    let tax = document.getElementById('tax').value;
+
+    // Add Decemal Check
+    if (income < 0 || tax > 100 || tax < 0 || income === 0 || tax === 0) {
+        alert('Wrong Values! Please correct!');
+    } else {
+
+        function calculation() {
+            let incomeTaxAmmount = (income/100*tax).toFixed(2);
+            let incomeBrutoAmmount = (income-incomeTaxAmmount).toFixed(2);
+    
+            document.getElementById('finalTax').innerHTML = incomeTaxAmmount;
+            document.getElementById('finalAmount').innerHTML = incomeBrutoAmmount;
+        }
+
+    }
+
+    calculation();
+}
+
+// $(document).ready(function() {
+//     $('form').submit(function(e) {
+//         e.preventDefault();
+//         // or return false;
+//     });
+// });
